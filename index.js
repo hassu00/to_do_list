7b966c5b95b952817bc167d53a41636436fd478f
hassu00 <rgbeasty@gmail.com>
2024-09-01 18:42:22 +0500

    this a updated version

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import inquirer from "inquirer";
 let todos = [];
-let condition = true;
-while (condition) {
-    let answer = await inquirer.prompt([
+async function addTask() {
+    // @ts-ignore
+    const answer = await inquirer.prompt([
         {
             type: "input",
             name: "task",
@@ -12,17 +12,80 @@ while (condition) {
                     return "Task cannot be empty.";
                 }
                 return true;
-            }
-        }
-    ]);
-    todos.push(answer.task);
-    let addAnotherTask = await inquirer.prompt([
+            },
+        },
         {
             type: "confirm",
             name: "addAnother",
             message: "Add another task?",
             default: false,
-        }
+        },
     ]);
-    condition = addAnotherTask.addAnother;
+    todos.push(answer.task);
+    if (answer.addAnother) {
+        await addTask();
+    }
 }
+async function updateIndex() {
+    // @ts-ignore
+    let updateIndex = await inquirer.prompt([
+        {
+            type: "list",
+            name: "updateIndex",
+            message: "Choose the task to update:",
+            choices: todos
+        },
+        {
+            type: "input",
+            name: "newTask",
+            message: "Enter the updated task:",
+        },
+    ]);
+    const index = todos.indexOf(updateIndex.updateIndex);
+    todos[index] = updateIndex.newTask;
+}
+async function deleteIndex() {
+    // @ts-ignore
+    let deleteIndex = await inquirer.prompt([{
+            type: "list",
+            name: "deleter",
+            message: "Choose the task to delete:",
+            choices: todos,
+        }]);
+    todos.splice(todos.indexOf(deleteIndex.deleter), 1);
+}
+async function viewList() {
+    console.log("Current tasks:", todos);
+}
+async function main() {
+    await addTask();
+    while (true) {
+        console.log(`current tasks list:`, todos);
+        // @ts-ignore
+        let answer1 = await inquirer.prompt([
+            {
+                name: "changes",
+                type: "list",
+                message: "Do you want to change something or review the todo list?",
+                choices: ["addmore", "update", "delete", "view list", "exit"],
+            },
+        ]);
+        switch (answer1.changes) {
+            case "addmore":
+                await addTask();
+                break;
+            case "update":
+                await updateIndex();
+                break;
+            case "delete":
+                await deleteIndex();
+                break;
+            case "viewList":
+                await viewList();
+                break;
+            case "exit":
+                return;
+        }
+    }
+}
+main();
